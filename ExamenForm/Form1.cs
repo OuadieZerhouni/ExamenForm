5060e0426e2c56adc235a9aa3cce649ef5c484b2
Wadie <ouadie.zerhouni@usmba.ac.ma>
2022-11-22 11:19:59 +0100

    test

--- ExamenForm/Form1.cs
+++ ExamenForm/Form1.cs
@@ -6,5 +6,10 @@ namespace ExamenForm
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ ExamenForm/Question.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenForm
+{
+    public abstract class Question
+    {
+        public void RepondreQuestion()
+        {
+            // TODO: implement
+        }
+
+        private int IdExamen;
+        private String QuestionType;
+        private String EnnonceQuestion;
+        private int NumQuestion;
+
+        public Question(int IdExamen, String EnnonceQuestion, int NumQuestion)
+        {
+            SetNumQuestion(NumQuestion);
+            SetIdExamen(IdExamen);
+            SetEcconceQuestion(EnnonceQuestion);
+
+        }
+
+        public void SetIdExamen(int Id)
+        {
+            this.IdExamen = Id;
+        }
+        public int GetIdExamen()
+        {
+            return this.IdExamen;
+        }
+        public void SetEcconceQuestion(String Ennonce)
+        {
+            this.EnnonceQuestion = Ennonce;
+        }
+        public String GetEcconceQuestion()
+        {
+            return this.EnnonceQuestion;
+        }
+        public void SetNumQuestion(int NumQuestion)
+        {
+            this.NumQuestion = NumQuestion;
+        }
+        public int GetNumQuestion()
+        {
+            return this.NumQuestion;
+        }
+    }
+}
