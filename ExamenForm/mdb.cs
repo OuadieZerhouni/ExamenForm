04b8d6d28b3e944df841e9dc0f4db26195a405a2
Wadie <ouadie.zerhouni@usmba.ac.ma>
2022-11-22 11:23:54 +0100

    test

--- ExamenForm/mdb.cs
+++ ExamenForm/mdb.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ExamenForm
 {
     internal class mdb
     {
+        static string chaine = @"Data Source=localhost;Initial Catalog=BDGED_copy;Integrated Security=True";
+        //"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\App_Data\VotreBD.mdf;Integrated Security=True;User Instance=True"
+        //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
     }
 }
