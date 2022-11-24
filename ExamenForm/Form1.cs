83542aca06317817d7db8e81f23105e22c7d18fc
OuadieZerhouni <Ouadie.zerhouni@usmba.ac.ma>
2022-11-24 22:00:42 +0100

    loadinitial() function

--- ExamenForm/Form1.cs
+++ ExamenForm/Form1.cs
@@ -2,10 +2,15 @@ namespace ExamenForm
 {
     public partial class Form1 : Form
     {
+    private Examen Exm;
+
+
         public Form1()
         {
             InitializeComponent();
+            loadInitial();
         }
+        
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -49,6 +54,12 @@ namespace ExamenForm
 
         private void Addbtn_Click(object sender, EventArgs e)
         {
+            if(titleLabel.Text == "Créer Exam")
+            {
+                Exm= new Examen(1, textBox2.Text, textBox1.Text);
+
+            }
+            
 
         }
 
@@ -70,6 +81,24 @@ namespace ExamenForm
         private void confirmbtn_Click(object sender, EventArgs e)
         {
 
+        }
+        private void loadInitial()
+        {
+            titleLabel.Visible = true;
+            titleLabel.Text = "Créer Exam";
+            selectCombo.Visible = false;
+            Question_type.Visible = false;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            Addbtn.Visible = true;
+            deletebtn.Visible = false;
+            modifybtn.Visible = false;
+            Cancelbtn.Visible = false;
+            confirmbtn.Visible = false;
+            label1.Text = "Exam theme";
+            label2.Text = "Exam date";
         }
     }
 }
